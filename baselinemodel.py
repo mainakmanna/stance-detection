0747d278c4c0aacfebc2335e30526812e41c88e5
rohandas14 <roh.das@outlook.com>
2018-08-09 19:44:39 +0530

    removing baseline.py and adding crossvalidation to baselinemodel.py

--- baselinemodel.py
+++ baselinemodel.py
@@ -1,154 +1,166 @@
-import nltk.data
-from gensim.models.keyedvectors import KeyedVectors
-from preprocess import *
+import tensorflow as tf
 import numpy as np
+import nltk.data
+import math
 import re
-import tensorflow as tf  
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split, KFold
+from preprocess import loadDataset
+from word2vec_training import loadWord2VecConvertedFromGlove, loadWord2VecOnGoogleDataset
 
-word_vectors = KeyedVectors.load_word2vec_format('./resources/GoogleNews-vectors-negative300.bin', binary=True)
+# Parameters
+learning_rate = 0.001
+epochs = 20
+batch_size = 32
+hidden_nodes = 128
+dropout = 0.2
+split_size = 10
+
+# Tensorflow Graph
+x = tf.placeholder(shape = ([None, 600]), dtype = tf.float64, name = 'x')
+y = tf.placeholder(shape = [None, 4], dtype = tf.float64, name = 'y')
+
+# Weights
+weights = {
+        'W1': tf.get_variable("W1", shape = [600, hidden_nodes], 
+                              dtype = tf.float64, initializer = tf.contrib.layers.xavier_initializer()),
+ 
+        'W2': tf.get_variable("W2", shape = [hidden_nodes, 4], 
+                              dtype = tf.float64, initializer = tf.contrib.layers.xavier_initializer()) 
+}
+
+#Biases
+biases = {
+        'b1': tf.get_variable("b1", shape = [1, hidden_nodes], 
+                              dtype = tf.float64, initializer = tf.constant_initializer(0)),
+                              
+        'b2': tf.get_variable("b2", shape = [1, 4], 
+                              dtype = tf.float64, initializer = tf.constant_initializer(0))
+}
+
+# Feedforward neural network model
+def forward_propagation(X):
+    hidden_layer = tf.matmul(X, weights['W1']) + biases['b1']
+    hidden_layer = tf.nn.relu(hidden_layer)
+    hidden_layer_dropout = tf.nn.dropout(hidden_layer, keep_prob = (1 - dropout))
+    out_layer = tf.matmul(hidden_layer_dropout, weights['W2']) + biases['b2']
+    
+    return out_layer
+
+# Construct the model
+y_hat = forward_propagation(x)
+
+# Cost and optimizer functions
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = y_hat, labels = y))
+optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cost)
+
+# Accuracy function
+correct_predicton = tf.equal(tf.argmax(y, 1), tf.argmax(y_hat, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_predicton, tf.float64))
+
+# Initializing the variables
+init = tf.global_variables_initializer()
 
 def clean(s):
-    return " ".join(re.findall(r'\w+', s, flags=re.UNICODE)).lower()
+    return " ".join(re.findall(r'\w+', s, flags = re.UNICODE)).lower()
 
-def get_data():
+def prepare_dataset():
+    print("Loading word2vec model...")
+    #word2vec_model = loadWord2VecConvertedFromGlove()
+    word2vec_model = loadWord2VecOnGoogleDataset()
+    print("Finished loading word2vec model.")
+    
+    print("Getting dataset...")
     headline_body_pairs, stances = loadDataset();
-    w = [None] * len(headline_body_pairs)
-    results = {'agree':0,'disagree':1,'discuss':2,'unrelated':3}
-    p = [None] * len(headline_body_pairs)
+    print("Finished getting dataset.")
+    
+    stance_labelencoder = LabelEncoder()
+    stances_label_encoded = stance_labelencoder.fit_transform(stances)
+    stances_label_encoded = stances_label_encoded.reshape(len(stances_label_encoded), 1)
+    onehotencoder = OneHotEncoder(sparse = False)
+    stances_onehotencoded = onehotencoder.fit_transform(stances_label_encoded)
 
+    headline_body_pairs_vec = [None] * len(headline_body_pairs)
+    
     for i in range(0,  len(headline_body_pairs)):
-        # Get the headline and body separately
+        
+        # getting the headline and body separately
         headline = headline_body_pairs[i][0]
         body = headline_body_pairs[i][1]
 
-        stance = stances[i]
-        stance = results[stance]
-        p[i] = stance
-
-        headline=clean(headline)
-        body=clean(body)
-    	
-    	 # tokenization of headline+body    
-        headlinevec = nltk.word_tokenize(headline)
-        bodyvec = nltk.word_tokenize(body)
+        # cleaning the headline and body
+        headline = clean(headline)
+        body = clean(body)
         
-        # avoiding the numbers
-        hlv = np.array([np.array(word_vectors[w]) if w in word_vectors.vocab else np.zeros((300,)) for w in headlinevec])
-        bv = np.array([np.array(word_vectors[w]) if w in word_vectors.vocab else np.zeros((300,)) for w in bodyvec])
+         # tokenizing of headline and body    
+        headline = nltk.word_tokenize(headline)
+        body = nltk.word_tokenize(body)
+        
+        # getting word vectors, replacing unknown words and numbers with zero vectors
+        headline_vec = np.array([np.array(word2vec_model[word]) if word in word2vec_model.vocab else np.zeros((300,)) for word in headline])
+        body_vec = np.array([np.array(word2vec_model[word]) if word in word2vec_model.vocab else np.zeros((300,)) for word in body])
         
         # columnwise mean
-        numpy_headlinevec = np.mean(np.array(hlv),axis=0)
-        numpy_bodyvec = np.mean(np.array(bv),axis=0)
+        headlinevec_mean = np.mean(np.array(headline_vec), axis=0)
+        bodyvec_mean = np.mean(np.array(body_vec), axis=0)
         
-        w[i] = np.concatenate((numpy_headlinevec,numpy_bodyvec),axis=0)
+        # concatenating the headline and body vectors
+        headline_body_pairs_vec[i] = np.concatenate((headlinevec_mean, bodyvec_mean),axis=0)
         
-    w = np.array(w)
-    p = np.eye(len(results))[p]
+    headline_body_pairs_vec = np.array(headline_body_pairs_vec)
+    
+    return headline_body_pairs_vec, stances_onehotencoded
 
-    return w,p
+def split_dataset(x, y):
+    X_train, X_dev, y_train, y_dev = train_test_split(x, y, test_size = 0.1, random_state = 42)
+    return X_train, X_dev, y_train, y_dev
 
-    
-def forward_propagation(ww,W1,b1,keep_prob,W2,b2):
-    h = tf.nn.relu(tf.matmul(ww, W1) + b1)
-    h_drop = tf.nn.dropout(h,keep_prob)
-    yhat = tf.matmul(h_drop, W2) + b2
-    return yhat
-
-def plotGraph(epochList, lossList, trainAccList, testAccList):
-    plt.figure(1)
-    plt.plot(epochList, trainAccList, 'r--', label='Train Accuracy')
-    plt.plot(epochList,testAccList,'g^',label='Test Accuracy' )
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.legend()
-    plt.figure(2)
-    plt.plot(epochList,lossList,'bs',label='loss')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.show()
-
-def main():
-    print('starting...')
-    w,p = get_data()
-    print('Got data...')
-    train_X, test_X, train_y, test_y = train_test_split(w, p, test_size=0.33, random_state=42)
-    # Create the tensorflow model
-    
-    print('Building model...')
-    tf.reset_default_graph()
-    
-    hidden_nodes = 128
-    keep_prob = tf.placeholder(tf.float64)
-    
-    # ww is the input matrix     
-    ww = tf.placeholder(shape=(None,w[0].shape[0]), dtype=tf.float64, name='ww')
-    
-    # pp is the correct labels (stances)
-    pp = tf.placeholder(shape=(None, 4), dtype=tf.float64, name='pp')
-    
-    W1 = tf.get_variable("W1", shape=[w[0].shape[0], hidden_nodes],
-               dtype=tf.float64,initializer=tf.contrib.layers.xavier_initializer())
-    W2 = tf.get_variable("W2", shape=[hidden_nodes, 4],
-               dtype=tf.float64,initializer=tf.contrib.layers.xavier_initializer())
-    
-    b1 = tf.get_variable("b1", shape=[1,hidden_nodes], dtype=tf.float64,
-                        initializer=tf.constant_initializer(0))
-    b2 = tf.get_variable("b2", shape=[1,4], dtype=tf.float64,
-                        initializer=tf.constant_initializer(0))
-    
-    # Forward propagation
-    yhat = forward_propagation(ww,W1,b1,keep_prob,W2,b2)
-    predict = tf.argmax(yhat,axis=1)
-    predict = tf.cast(predict, tf.float64)
-    
-    # Backward Propagation
-    cost    = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=pp, logits=yhat))
-    updates  = tf.train.AdamOptimizer(1e-4).minimize(cost)
-    
-    ######################################
-    
-    # Create operation which will initialize all variables
-    init = tf.global_variables_initializer()
-    
+def train(session, X_train, y_train):
     # Configure GPU not to use all memory
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
-    
-    # Start a new tensorflow session and initialize variables
-    sess = tf.InteractiveSession(config=config)
-    sess.run(init)
-    
-    # These lists are for storing data later used in plotting
-    epochList = []
-    lossList = []
-    trainAccList = []
-    testAccList = []
-    for epoch in range(10):
-        avg_cost = 0.0
-        loss = 0
-        for i in range(len(train_X)):
-            _,loss = sess.run([updates,cost], feed_dict={ww: np.matrix(train_X[i]), pp: np.matrix(train_y[i]),keep_prob : 0.2});
-            avg_cost += loss
-        avg_cost = avg_cost /  len(train_X)
-        train_accuracy = np.mean(np.argmax(train_y, axis=1) ==
-                                 sess.run(predict, feed_dict={ww: train_X, pp: train_y,keep_prob:0.8}))
-        test_accuracy  = np.mean(np.argmax(test_y, axis=1) ==
-                                 sess.run(predict, feed_dict={ww: test_X, pp: test_y,keep_prob:0.8}))
-        print("Epoch = %d, train accuracy = %.2f%%, test accuracy = %.2f%% , loss = %.2f%% "
-              % (epoch + 1, 100. * train_accuracy, 100. * test_accuracy, avg_cost*100))
         
-        epochList.append(epoch+1)
-        lossList.append(avg_cost*100.)
-        trainAccList.append(train_accuracy*100.)
-        testAccList.append(test_accuracy*100.)
-
-    plotGraph(epochList, lossList, trainAccList, testAccList)
-    
-    sess.close()
-    
-if __name__ == '__main__':
-    main()
+    session.run(init)
+    print("\n")
+    total_batch = int(math.ceil(len(X_train)/batch_size))
+    for epoch in range(epochs):
+        avg_cost = 0
+        loss = 0
+        start, end = 0, batch_size
+        for i in range(total_batch):
+            batch_x = X_train[start:end]
+            batch_y = y_train[start:end]
+            
+            _, loss = session.run([optimizer, cost], feed_dict = {x: batch_x, y: batch_y})
+            avg_cost += loss
+            start += batch_size
+            if i == total_batch - 2:
+                end = len(X_train)
+            else:
+                end += batch_size
+        avg_cost = avg_cost/total_batch
+        train_accuracy = session.run(accuracy, feed_dict = {x: X_train, y: y_train})
+        print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost), "accuracy =", "{:.3f}".format(train_accuracy))
+   
+def cross_validate(session, X_train, X_dev, y_train, y_dev):
+    results = []
+    kf = KFold(n_splits = split_size)
+    for train_idx, val_idx in kf.split(X_train, y_train):
+        train_x = X_train[train_idx]
+        train_y = y_train[train_idx]
+        val_x = X_train[val_idx]
+        val_y = y_train[val_idx]
+        train(session, train_x, train_y)
+        results.append(session.run(accuracy, feed_dict={x: val_x, y: val_y}))
+    test_accuracy = session.run(accuracy, feed_dict={x: X_dev, y: y_dev})
+    return results, test_accuracy
+     
+def main():
+    x, y = prepare_dataset()
+    X_train, X_dev, y_train, y_dev = split_dataset(x, y)
+    with tf.Session() as session:
+        #train(session, X_train, y_train)
+        result, test_accuracy = cross_validate(session, X_train, X_dev, y_train, y_dev)
+        print("\n")
+        print("Cross-validation result: ", result)
+        print("Test accuracy: ", test_accuracy)
