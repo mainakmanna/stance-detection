b8ccfba4544d69ba127c3999328d941d259efbdd
Mainak Manna <moinak.manna@gmail.com>
2018-10-27 15:38:42 +0530

    Adds split dataset as mentioned in FNC.

--- split_data.py
+++ split_data.py
@@ -12,3 +12,7 @@ dataset_stances = pd.read_csv("./dataset/train_stances.csv")
 merged = pd.merge(dataset_bodies, dataset_stances, on="Body ID")
 training_bodies = pd.read_csv("./splits/training_ids.csv")
 training_bodies_final = merged.loc[merged['Body ID'].isin(training_bodies['Body ID'])]
+train_bodies_csv = training_bodies_final[['Body ID','articleBody']].copy()
+train_stances_csv = training_bodies_final[['Headline','Body ID','Stance']].copy()
+train_bodies_csv.to_csv('./dataset/train_bodies1.csv', index=False)
+train_stances_csv.to_csv('./dataset/train_stances1.csv', index=False)
